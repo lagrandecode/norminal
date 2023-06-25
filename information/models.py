adfe0c90c60823cb7423fe8842bd397bddc92fd8
lagrandecode <seunogunmolufirst1@gmail.com>
2023-06-25 19:35:27 +0100

    "commit"

--- information/models.py
+++ information/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Information(models.Model):
+    surname = models.CharField(max_length=200)
+    name = models.Charfield(max_length=200)
+    
+    
