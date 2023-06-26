e45ec028b09a8f2f8cf02345b2ac04fe8719d642
lagrandecode <seunogunmolufirst1@gmail.com>
2023-06-26 22:13:55 +0100

    "commit"

--- information/models.py
+++ information/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class Information(models.Model):
     surname = models.CharField(max_length=200)
-    name = models.Charfield(max_length=200)
+    name = models.CharField(max_length=200)
+    other_name = models.CharField(max_length=200)
     
     
