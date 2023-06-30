d8f8078d0d4217e1e9d9c0c7827e6d8f1d75e790
lagrandecode <seunogunmolufirst1@gmail.com>
2023-06-30 17:47:07 +0100

    "commit"

--- information/models.py
+++ information/models.py
@@ -27,9 +27,29 @@ class Information(models.Model):
     civil_service_number = models.CharField(max_length=20)
     mepb_file_number = models.CharField(max_length=20)
     present_post = models.CharField(max_length=20)
+    mdas_posted = models.PositiveIntegerField()
     phone_number = models.CharField(max_length=20)
     profile_pic = models.ImageField(upload_to='images/',null=True,blank=True)
     status = models.CharField(max_length=20,choices=STATUS)
+
+
+class Department(models.Model):
+    name = models.CharField(max_length=120)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class Designation(models.model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
 
     
     
