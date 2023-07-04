880e9d2698c0afbf98766bd405dc66e9cc0080b2
lagrandecode <seunogunmolufirst1@gmail.com>
2023-07-04 21:45:00 +0100

    "commit"

--- information/models.py
+++ information/models.py
@@ -1,14 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
 
+
+# from phonenumber_field.modelfields import PhoneNumberField
+from django.db import models
+from django.db import IntegrityError
 # Create your models here.
 
 
-class Information(models.Model):
+class MyUserManager(BaseUserManager):
+    def create_user(self,email,password,**extra_fields):
+        if not email:
+            raise ValueError('User must have an email address')
+        email = self.normalize_email(email)
+        new_user = self.model(email=email,**extra_fields)
+        new_user.set_password(password)
+        new_user.save()
+        return new_user
+
+    def create_superuser(self,email,password,**extra_fields):
+        extra_fields.setdefault('is_staff',True)
+        extra_fields.setdefault('is_superuser',True)
+        extra_fields.setdefault('is_active',True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_("Superuser should have is_staff as True"))
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_("Superuser should have is_superuser as True"))
+
+        if extra_fields.get('is_active') is not True:
+            raise ValueError(_("Superuser should have is_active as True"))
+
+        return self.create_user(email,password,**extra_fields)
+
+
+class Information(AbstactUser):
+ 
     STATUS = [('ACTIVE','ACTIVE'),('RETIRED','RETIRED'),('RESIGNED','RESIGNED'),('OTHERS','OTHERS')]
     GENDER = (('MALE','M'),('FEMALE','F'))
     LOCAL_GOVERNEMNT = (('Agege Local Government','Agege Local Government'),('Ajeromi-Ifelodun Local Government','Ajeromi-Ifelodun Local Government'),('Alimosho Local Government','Alimosho Local Government'),('Amuwo-Odofin Local Government','Amuwo-Odofin Local Government'),
     ('Apapa Local Government','Apapa Local Government'),('Badagry Local Government','Badagry Local Government'),('Epe Local Government','Epe Local Government'),('Eti-Osa Local Government','Eti-Osa Local Government'),('Ibeju-Lekki Local Government','Ibeju-Lekki Local Government'),('Ifako-Ijaiye Local Government','Ifako-Ijaiye Local Government'),('Ikeja Local Government','Ikeja Local Government'),('Ikorodu Local Government','Ikorodu Local Government'),('Kosofe Local Government','Kosofe Local Government','Lagos Island Local Government','Lagos Island Local Government'),('Lagos Mainland Local Government','Lagos Mainland Local Government'),('Mushin Local Government','Mushin Local Government'),('Ojo Local Government','Ojo Local Government'),('Oshodi-Isolo Local Government','Oshodi-Isolo Local Government'),('Shomolu Local Government','Shomolu Local Government'),('Surulere Local Government','Surulere Local Government'),('outside Nigeria','outside Nigeria'))
     GRADE = (('GL1','GL1'),('GL2','GL2'),('GL3','GL3'),('GL4','GL4'),('GL5','GL5'),('GL6','GL6'),('GL7','GL7'),('GL8','GL8'),('GL9','GL9'),('GL10','GL10'),('GL12','GL12'),('GL13','GL13'),('GL14','GL14'),('GL15','GL15'),('GL16','GL16'),('GL17','GL17'))
+    username = None  # Removed username, using email instead
+    email = models.EmailField(unique=True)
     surname = models.CharField(max_length=200)
     name = models.CharField(max_length=200)
     other_name = models.CharField(max_length=200)
@@ -29,6 +65,16 @@ class Information(models.Model):
     profile_pic = models.ImageField(upload_to='images/',null=True,blank=True)
     description = models.CharField(max_length=3000)
     status = models.CharField(max_length=20,choices=STATUS)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+
+
+
+
+
 
 
 class Department(models.Model):
