41cbe43d873985544867d4b8f183436e17234662
lagrandecode <seunogunmolufirst1@gmail.com>
2023-07-27 07:05:33 +0100

    "commit"

--- norminal/settings.py
+++ norminal/settings.py
@@ -113,6 +113,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+AUTH_USER_MODEL = 'information.AbstractUser'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
