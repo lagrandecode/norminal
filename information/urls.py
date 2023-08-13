8368d3d8bf30a2ea4062e8b9906989e9f4b03b70
lagrandecode <seunogunmolufirst1@gmail.com>
2023-08-13 23:55:50 +0100

    "commit"

--- information/urls.py
+++ information/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('',views.SignupView.as_view(),name='signup'),
     path('login/',views.LoginView.as_view(),name='login'),
     path('info/',views.UserInfo.as_view(),name='info'),
+    path('staff/',views.StaffUser.as_view(),name='staff'),
 
 ]
