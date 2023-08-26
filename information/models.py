8e71884b2328ad53c4d7b60410f0a25bea3b9aa4
lagrandecode <seunogunmolufirst1@gmail.com>
2023-08-26 21:58:46 +0100

    "commit"

--- information/models.py
+++ information/models.py
@@ -137,9 +137,6 @@ class Staff(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return self.admin.name
-
 
 
 class StaffNotification(models.Model):
